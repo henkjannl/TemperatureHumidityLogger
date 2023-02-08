5bd3083898e720f4efd98a906bd0c92d954f1853
HenkJan van der Pol | Bond3D <hj.vanderpol@bond3d.com>
2023-02-08 19:32:53 +0100

    Update Test03_keyboardCallback.ino

--- 03 Software/Tests/Test03_keyboardCallback/Test03_keyboardCallback.ino
+++ 03 Software/Tests/Test03_keyboardCallback/Test03_keyboardCallback.ino
@@ -1,51 +1,12 @@
-/*
-  Name:        keyboards.ino
-  Created:     20/06/2020
-  Author:      Tolentino Cotesta <cotestatnt@yahoo.com>
-  Description: a more complex example that do:
-             1) if a "/inline_keyboard1" text message is received, show the inline custom keyboard 1,
-                otherwise reply the sender with hint message
-             2) if "LIGHT ON" inline keyboard button is pressed turn on the LED and show a message
-             3) if "LIGHT OFF" inline keyboard button is pressed, turn off the LED and show a message
-             4) if "Button 1" inline keyboard button is pressed show a "modal" message with message box
-             5) if "Button 2" inline keyboard button is pressed show a message  
-*/
-
-/* 
-  Set true if you want use external library for SSL connection instead ESP32@WiFiClientSecure 
-  For example https://github.com/OPEnSLab-OSU/SSLClient/ is very efficient BearSSL library.
-  You can use AsyncTelegram2 even with other MCUs or transport layer (ex. Ethernet)
-*/ 
-#define USE_CLIENTSSL false
-  
 #include <AsyncTelegram2.h>
 
 // Timezone definition
 #include <time.h>
 #define MYTZ "CET-1CEST,M3.5.0,M10.5.0/3"
 
-#ifdef ESP8266
-  #include <ESP8266WiFi.h>
-  BearSSL::WiFiClientSecure client;
-  BearSSL::Session   session;
-  BearSSL::X509List  certificate(telegram_cert);
-  
-#elif defined(ESP32)
-  #include <WiFi.h>
-  #if USE_CLIENTSSL
-    #include <WiFiClient.h>
-    #include <SSLClient.h>  
-    #include "tg_certificate.h"
-    WiFiClient base_client;
-    SSLClient client(base_client, TAs, (size_t)TAs_NUM, A0, 1, SSLClient::SSL_ERROR);
-  #else
-    #include <WiFiClientSecure.h>
-    WiFiClientSecure client;  
-  #endif
-#endif
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
 
-
-AsyncTelegram2 myBot(client);
 /*
 const char* ssid  =  "xxxxxxxxx";     // SSID WiFi network
 const char* pass  =  "xxxxxxxxx";     // Password  WiFi network
@@ -53,36 +14,40 @@ const char* token =  "xxxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";  // Telegram t
 */
 #include "MyCredentials.h"
 
-InlineKeyboard myInlineKbd1, myInlineKbd2; // inline keyboards object helper
+WiFiClientSecure client;  
+AsyncTelegram2 myBot(client);
 
-#define LIGHT_ON_CALLBACK  "lightON"   // callback data sent when "LIGHT ON" button is pressed
-#define LIGHT_OFF_CALLBACK "lightOFF"  // callback data sent when "LIGHT OFF" button is pressed
-#define BUTTON1_CALLBACK   "Button1"   // callback data sent when "Button1" button is pressed
-#define BUTTON2_CALLBACK   "Button2"   // callback data sent when "Button1" button is pressed
+InlineKeyboard inlineKeyboard; // inline keyboards object helper
+
+  inlineKeyboard.addButton("Download logfile",  ,  KeyboardButtonQuery, onDownload);
+  inlineKeyboard.addButton("Clear logfile",     , KeyboardButtonQuery, onClearLog);
+  // add a new empty button row
+  inlineKeyboard.addRow();
+  inlineKeyboard.addButton("Update status",     CB_STATUS,    KeyboardButtonQuery, onStatus);
+
+
+#define CB_DOWNLOAD  "cbDownload"
+#define CB_CLEAR_LOG "cbClearLog" 
+#define CB_STATUS    "cbStatus"
 
 const uint8_t LED = 4;
 
 // Callback functions definition for inline keyboard buttons
-void onPressed(const TBMessage &queryMsg){
+void onDownload(const TBMessage &queryMsg){
   digitalWrite(LED, HIGH);
-  Serial.printf("\nON button pressed (callback);\nQueryId: %s\n\n", queryMsg.callbackQueryID);
+  Serial.printf("Download function to be implemented here\nQueryId: %s ChatID: %d\n", queryMsg.callbackQueryID, queryMsg.chatId);
   myBot.endQuery(queryMsg, "Light on", true);
 }
 
-void offPressed(const TBMessage &queryMsg){
+void onClearLog(const TBMessage &queryMsg){
   digitalWrite(LED, LOW);
-  Serial.printf("\nOFF button pressed (callback); \nQueryId: %s\n\n", queryMsg.callbackQueryID);
+  Serial.printf("Clear logfile to be implemented here\nQueryId: %s ChatID: %d\n", queryMsg.callbackQueryID, queryMsg.chatId);
   myBot.endQuery(queryMsg, "Light on", false);
 }
 
-void button1Pressed(const TBMessage &queryMsg){
-  Serial.printf("\nButton 1 pressed (callback); \nQueryId: %s\n\n", queryMsg.callbackQueryID);
-  myBot.endQuery(queryMsg, "You pressed Button 1", true);
-}
-
-void button2Pressed(const TBMessage &queryMsg){
-  Serial.printf("\nButton 2 pressed (callback); \nQueryId: %s\n\n", queryMsg.callbackQueryID);
-  myBot.endQuery(queryMsg, "You pressed Button 2", false);
+void onStatus(const TBMessage &queryMsg){
+  Serial.printf("Status update to be implemented here\nQueryId: %s ChatID: %d\n", queryMsg.callbackQueryID, queryMsg.chatId);
+  myBot.endQuery(queryMsg, "Status", true);
 }
 
 void setup() {
@@ -101,53 +66,30 @@ void setup() {
     delay(500);
   }
 
-#ifdef ESP8266
-  // Sync time with NTP, to check properly Telegram certificate
-  configTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
-  //Set certficate, session and some other base client properies
-  client.setSession(&session);
-  client.setTrustAnchors(&certificate);
-  client.setBufferSizes(1024, 1024);
-#elif defined(ESP32)
   // Sync time with NTP
   configTzTime(MYTZ, "time.google.com", "time.windows.com", "pool.ntp.org");
-  #if USE_CLIENTSSL == false
-    client.setCACert(telegram_cert);
-  #endif
-#endif
+  client.setCACert(telegram_cert);
 
   // Set the Telegram bot properties
   myBot.setUpdateTime(1000);
   myBot.setTelegramToken(token);
 
   // Check if all things are ok
-  Serial.print("\nTest Telegram connection... ");
+  Serial.print("Test Telegram connection... ");
   myBot.begin() ? Serial.println("OK") : Serial.println("NOK");
   Serial.print("Bot name: @");
   Serial.println(myBot.getBotName());
 
   // Add sample inline keyboard
-  // add a button that will turn on LED on pin assigned
-  myInlineKbd1.addButton("ON",  LIGHT_ON_CALLBACK, KeyboardButtonQuery, onPressed);
-  // add a button that will turn off LED on pin assigned
-  myInlineKbd1.addButton("OFF", LIGHT_OFF_CALLBACK, KeyboardButtonQuery, offPressed);
+  inlineKeyboard.addButton("Download logfile",  CB_DOWNLOAD,  KeyboardButtonQuery, onDownload);
+  inlineKeyboard.addButton("Clear logfile",     CB_CLEAR_LOG, KeyboardButtonQuery, onClearLog);
   // add a new empty button row
-  myInlineKbd1.addRow();
-  // add a button that will open browser pointing to this GitHub repository
-  myInlineKbd1.addButton("GitHub", "https://github.com/cotestatnt/AsyncTelegram2/", KeyboardButtonURL);
-  Serial.printf("Added %d buttons to keyboard\n", myInlineKbd1.getButtonsNumber());
-  
-  // Add another inline keyboard
-  myInlineKbd2.addButton("Button 1", BUTTON1_CALLBACK, KeyboardButtonQuery, button1Pressed);
-  myInlineKbd2.addButton("Button 2", BUTTON2_CALLBACK, KeyboardButtonQuery, button2Pressed);
-  Serial.printf("Added %d buttons to keyboard\n", myInlineKbd2.getButtonsNumber());
-  
+  inlineKeyboard.addRow();
+  inlineKeyboard.addButton("Update status",     CB_STATUS,    KeyboardButtonQuery, onStatus);
+    
   // Add pointer to this keyboard to bot (in order to run callback function)
-  myBot.addInlineKeyboard(&myInlineKbd1);
-  myBot.addInlineKeyboard(&myInlineKbd2);
+  myBot.addInlineKeyboard(&inlineKeyboard);
 }
-
-
 
 void loop() {
 
@@ -159,7 +101,7 @@ void loop() {
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
   }
 
-  printHeapStats();
+  //printHeapStats();
   
   // a variable to store telegram message data
   TBMessage msg;
@@ -174,15 +116,12 @@ void loop() {
       case MessageText :
         // received a text message
         tgReply = msg.text;
-        Serial.print("\nText message received: ");
+        Serial.print("Text message received: ");
         Serial.println(tgReply);
 
         if (tgReply.equalsIgnoreCase("/inline_keyboard1")) {          
-          myBot.sendMessage(msg, "This is inline keyboard 1:", myInlineKbd1);          
+          myBot.sendMessage(msg, "This is inline keyboard 1:", inlineKeyboard);          
         }        
-        else if (tgReply.equalsIgnoreCase("/inline_keyboard2")) {          
-          myBot.sendMessage(msg, "This is inline keyboard 2:", myInlineKbd2);          
-        } 
         else {
           // write back feedback message and show a hint
           String text = "You write: \"";
@@ -213,16 +152,9 @@ void printHeapStats() {
   static uint32_t infoTime;
   if (millis() - infoTime > 10000) {
     infoTime = millis();
-#ifdef ESP32
-    //heap_caps_print_heap_info(MALLOC_CAP_DEFAULT);
-    Serial.printf("\n%02d:%02d:%02d - Total free: %6d - Max block: %6d",
-      tInfo.tm_hour, tInfo.tm_min, tInfo.tm_sec, heap_caps_get_free_size(0), heap_caps_get_largest_free_block(0) );
 
-#elif defined(ESP8266)
-    uint32_t free;
-    uint16_t max;
-    ESP.getHeapStats(&free, &max, nullptr);
-    Serial.printf("\nTotal free: %5d - Max block: %5d", free, max);
-#endif
+  //heap_caps_print_heap_info(MALLOC_CAP_DEFAULT);
+  Serial.printf("\n%02d:%02d:%02d - Total free: %6d - Max block: %6d",
+    tInfo.tm_hour, tInfo.tm_min, tInfo.tm_sec, heap_caps_get_free_size(0), heap_caps_get_largest_free_block(0) );
   }
 }
